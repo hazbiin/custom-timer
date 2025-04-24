40fb26b435def9d31028aa28b21736000fc2e0c7
hasbin <hzbi717@gmail.com>
2025-04-24 09:48:46 +0530

    updated object creation of existing and new timer

--- script.js
+++ script.js
@@ -5,27 +5,28 @@ const timerBtnIcon = timerBtn.querySelector(".timer-path");
 const timerBtnText = timerBtn.querySelector(".timer-btn-text");
 
 const timer = document.querySelector(".timer");
+const timerName = document.querySelector(".timer-name");
 const timeLogList = document.querySelector(".time-log-list");
 
-const timeLogs = JSON.parse(localStorage.getItem("timeLogs")) || [];
-console.log(timeLogs);
+const timers = JSON.parse(localStorage.getItem("timers")) || [];
+console.log(timers);
 
 
 // initial loading from localstorage
 displayInitialLogs();
 function displayInitialLogs() {
-    timeLogs.forEach(timeLog => {
+    timers.forEach(timer => {
         const li = document.createElement("li");
             li.innerHTML = `
                      <div class="list-item-container">
                         <div class="new-timer-section">
-                            <button class="timer-btn new-timer-btn">
+                            <button class="timer-btn new-timer-btn" data-timer-id="${timer.timerId}">
                                 <svg class="timer-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path class="timer-path" d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg>
-                                <span class="timer-btn-text">Timer ${timeLog.timeLogId}</span>
+                                <span class="timer-btn-text">${timer.timerName}</span>
                             </button>
                         </div>
                         <div class="new-timer-total-section">
-                            <span class="timer-total">${timeLog.totalTime}</span>
+                            <span class="timer-total">${timer.timerTotalTime}</span>
                         </div>
                     </div>
             `;
@@ -38,6 +39,8 @@ function displayInitialLogs() {
     })
 }
 
+
+
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -47,6 +50,9 @@ let timerState = "stopped";
 let startTime = null;
 let endTime = null;
 let totalTime = null;
+let timerTotalTime = null;
+
+let newTimerBtnId = null;
 
 let timerCount = 0;
 timerBtn.addEventListener("click", () => {
@@ -57,7 +63,7 @@ timerBtn.addEventListener("click", () => {
         startTime = new Date();
 
         // setting timer count
-        if(timeLogs.length > 0) {
+        if(timers.length > 0) {
             const lastLi = document.querySelector(".time-log-list").lastChild;
             const previousTimer = lastLi.querySelector(".timer-btn-text");
             const [text, count] = previousTimer.textContent.split(" ");
@@ -68,7 +74,12 @@ timerBtn.addEventListener("click", () => {
         }else {
             timerCount++;
         }
-        
+
+
+        // getting timer id to later update the same timer logs
+
+
+
 
         // running timer
         intervalId = setInterval(() => {
@@ -90,24 +101,25 @@ timerBtn.addEventListener("click", () => {
         timerBtnText.textContent = "Stop";
         timerBtnIcon.setAttribute("d", "m798-274-60-60q11-27 16.5-53.5T760-440q0-116-82-198t-198-82q-24 0-51 5t-56 16l-60-60q38-20 80.5-30.5T480-800q60 0 117.5 20T706-722l56-56 56 56-56 56q38 51 58 108.5T840-440q0 42-10.5 83.5T798-274ZM520-552v-88h-80v8l80 80ZM792-56l-96-96q-48 35-103.5 53.5T480-80q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-60 18.5-115.5T192-656L56-792l56-56 736 736-56 56ZM480-160q42 0 82-13t75-37L248-599q-24 35-36 75t-12 84q0 116 82 198t198 82ZM360-840v-80h240v80H360Zm83 435Zm113-112Z");
 
-        const li = document.createElement("li");
-        li.innerHTML = `
-                 <div class="list-item-container">
-                    <div class="new-timer-section">
-                        <button class="timer-btn new-timer-btn">
-                            <svg class="timer-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path class="timer-path" d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg>
-                            <span class="timer-btn-text">Timer ${timerCount}</span>
-                        </button>
+        if(seconds === 0 && minutes === 0 && hours === 0 ) {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                    <div class="list-item-container">
+                        <div class="new-timer-section">
+                            <button class="timer-btn new-timer-btn" data-timer-id="${timerCount}">
+                                <svg class="timer-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path class="timer-path" d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"/></svg>
+                                <span class="timer-btn-text">Timer ${timerCount}</span>
+                            </button>
+                        </div>
+                        <div class="new-timer-total-section">
+                            <span class="timer-total">-- -- --</span>
+                        </div>
                     </div>
-                    <div class="new-timer-total-section">
-                        <span class="timer-total">-- -- --</span>
-                    </div>
-                </div>
-        `;
-        timeLogList.appendChild(li);
-
+            `;
+            timeLogList.appendChild(li);
+        }
         
-
+        
     }else {
 
         // stopping timer
@@ -118,17 +130,75 @@ timerBtn.addEventListener("click", () => {
 
         // getting total time
         const timeDiffInSeconds = Math.floor((endTime - startTime)/ 1000);
-        totalTime = convertSecondsToTimeFormat(timeDiffInSeconds)
+        totalTime = convertSecondsToTimeFormat(timeDiffInSeconds);
 
+    
         // storing to localstorage
-        timeLogs.push({
-            timeLogId: timerCount,
-            startTime,
-            endTime,
-            totalTime
-        });
+        if(timer.length === 0 || newTimerBtnId === null) {
+            console.log("--------new timer additon---------");
+            timers.push({
+                timerId: timerCount,
+                timerName : `Timer ${timerCount}`,
+                timeLogs : [
+                    {
+                        timeLogId: Date.now(),
+                        startTime,
+                        endTime,
+                        totalTime
+                    }
+                ],
+                timerTotalTime: totalTime
+            })
+        }else if(timer.length !== 0 && newTimerBtnId !== null) {
+            console.log("---------editing existing timer----------");
+            timers.forEach(timer => {
+                if(timer.timerId === Number(newTimerBtnId)){
+                    timer.timeLogs.push({
+                        timeLogId: Date.now(),
+                        startTime,
+                        endTime,
+                        totalTime
+                    })
+                }
+            })
+        }
 
-        localStorage.setItem("timeLogs", JSON.stringify(timeLogs));
+
+        // ////////adding all totals from the timelogs array.
+        timers.forEach(timeLog => {
+
+            timeLog.timeLogs.forEach(tl => {
+                const [h, m, s] = tl.totalTime.split(":");
+                console.log( "hreeereeeeeee",Number(h), Number(m), Number(s))
+            })
+        })
+
+
+
+        
+        
+       
+        
+        
+
+        
+        
+
+
+        
+
+
+
+        // ///////////////old implementation/////////////
+        // timers.push({
+        //     timeLogId: timerCount,
+        //     startTime,
+        //     endTime,
+        //     totalTime
+        // });
+
+
+        localStorage.setItem("timers", JSON.stringify(timers));
 
         // making necessary changes
         timerState = "stopped";
@@ -136,38 +206,71 @@ timerBtn.addEventListener("click", () => {
         timerBtnIcon.setAttribute("d", "M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z");
 
         timer.textContent = "00:00:00";
-
         seconds = 0;
         minutes = 0;
         hours = 0;
 
 
+
+
+        // /////////////////here we need a if else. for dom updation of existing or new timer
+        // const liToUpdate = null;
+        // if(document.querySelector(".timer-total"))
+
         const liToUpdate = document.querySelector(".time-log-list").lastChild;
         const timerTotal = liToUpdate.querySelector(".timer-total");
         timerTotal.textContent = totalTime; 
+
+        
+        // const newliToUpdate = document.querySelector(".timer-total");
+        // console.log(newliToUpdate)
+
+       
+
+        
+
+
+        // const newTimerBtns = document.querySelectorAll(".new-timer-btn");
+        // console.log(newTimerBtns);
+        // newTimerBtns.forEach(newTimerBtn => {
+        //     newTimerBtn.addEventListener("click" , () => {
+        //         newTimerFunciton();
+        //     })
+        // });
     }
 })
 
 
+// console.log("function called");
+
+const newTimerBtns = document.querySelectorAll(".new-timer-btn");
+let newTimerState = "stopped";
+
+newTimerBtns.forEach((newTimerBtn) => {
+    newTimerBtn.addEventListener("click", () => {
+
+        if(timerState !== "started") {
+            const totalTimeContainer = newTimerBtn.closest(".list-item-container").querySelector(".timer-total");
+            const newTime = totalTimeContainer.textContent;
 
 
-// const newTimerBtns = document.querySelectorAll(".new-timer-btn");
-// console.log(newTimerBtns);
+            newTimerBtnId = newTimerBtn.dataset.timerId;
 
-// newTimerBtns.forEach((newTimerBtn) => {
-//     newTimerBtn.addEventListener("click")
-// })
-
-
+            timer.textContent = newTime;
+            timerName.textContent = `Timer ${newTimerBtnId}`
+            timerBtnText.textContent = "Resume";
+            totalTimeContainer.textContent = "-- -- --";
 
 
 
-
-
-
-
-
-
+            let [h, m, s] = newTime.split(":");
+            seconds = Number(s);
+            minutes = Number(m);
+            h = Number(h);
+        }   
+        
+    })
+})
 
 
 
@@ -186,6 +289,39 @@ function convertSecondsToTimeFormat(totalSeconds){
 
 
 
-// ///////////////resume functionality ///////////////////////
 
-
+// final object iam gonna make
+// timell = [
+    //     {
+    //         id,
+    //         timerName: timer1,
+    //         timelogs : [
+    //             {
+    //                 id,
+    //                 startTime,
+    //                 endTime,
+    //                 totalTime
+    //             }
+    //         ],
+    //         totalTime: "add all"
+    //     },
+    //     {
+    //         id,
+    //         timerName: timer2,
+    //         timeLogs : [
+    //             {
+    //                 id,
+    //                 startTime,
+    //                 endTime,
+    //                 totalTime
+    //             },
+    //             {
+    //                 id,
+    //                 startTime,
+    //                 endTime,
+    //                 totalTime
+    //             }
+    //         ],
+    //         totalTime: "add all"
+    //     }
+    // ]
